108c623b4b71ca5f09a7058344464098d013123e
jomarusoup <sigojy000@gmail.com>
2025-01-27 13:32:33 +0900

    pyupb로 30일 일봉 데이터 가져는 로직 추가

--- mvp.py
+++ mvp.py
@@ -7,5 +7,7 @@ load_dotenv()
 
 # 1. upbit 차트 데이터 가져오기(30일 일봉 데이터)
 import pyupbit
-df = pyupbit.get_ohlcv("KRW-BTC")
+df = pyupbit.get_ohlcv("KRW-BTC", count=30, interval="day")
 print(df.tail())
+
+# 2. AI에게 데이터 제공하고 예측 결과 받기
